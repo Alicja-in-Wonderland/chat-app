4710d6d298b936ec9a708d73544dcdb6ca2c217f
Alicja <alanotowska@yahoo.co.uk>
2023-09-02 22:05:34 +0200

    Creates Chat screen component

---
+++ components/Chat.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Chat = ({ route, navigation }) => {
+    //Gets name and colour selection from Start component
+    const { name } = route.params;
+    const { colour } = route.params;
+
+    useEffect(() => {
+        navigation.setOptions({ title: name });
+    }, []);
+
+    return (
+        <View style={[styles.container, { backgroundColor: colour }]}>
+            <Text style={styles.textColour}>Welcome to the chatroom!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textColour: {
+        color: 'white'
+    }
+});
+
+export default Chat;
